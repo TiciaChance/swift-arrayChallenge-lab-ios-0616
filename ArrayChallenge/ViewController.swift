9282f6a0944f0238d7581257843b40f5724ef1b8
TiciaChance <laticia.chance@flatironschool.com>
2016-07-08 10:07:10 -0400

    no tests but i'm pretty sure this is right

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,15 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        let numbers: [Int] = [6, 4, 12, 4]
+        let food: [String] = ["Bananas", "Apples", "Eggs", "Rolls"]
         
-        
+        makeShoppingList(food, quantityOfItems: numbers)
     }
-    
-    
-    
 }
+
+//the first argument of the function will have no external name when it is called - which happens by default
+func makeShoppingList(items: [String], quantityOfItems: [Int]) -> [String] {
+    
+    var emptyArray: [String] = []
+    
+    for (index, theItem) in items.enumerate() {
+        let itemAndQuantity = ("\(index). \(theItem) -  \(quantityOfItems[index])")
+        emptyArray.append(itemAndQuantity)
+    }
+    return emptyArray
+}
+
+
+//let result = [
+//    ["merge", "me"],
+//    ["We", "shall", "unite"],
+//    ["magic"]
+//    ].flatMap { $0 }
+
